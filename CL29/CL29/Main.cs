3e873c313868be0756661ad96d83a1459e016d50
Romain Dolbeau <romain.dolbeau@atos.net>
2019-07-23 13:56:45 +0200

    Add alternate XP progression, with support for level up to 64

--- CL29/CL29/Main.cs
+++ CL29/CL29/Main.cs
@@ -12,9 +12,26 @@ using System.Collections.Generic;
 
 namespace CL29
 {
+    public class Settings : UnityModManager.ModSettings
+    {
+        public bool enableLinearScale = false;
+        public int maxLevel = Main.normalMax;
+
+        public override void Save(UnityModManager.ModEntry modEntry)
+        {
+            Save(this, modEntry);
+        }
+    }
     public class Main
     {
         public static UnityModManagerNet.UnityModManager.ModEntry.ModLogger logger;
+        public static UnityModManager.ModEntry ModEntry;
+        public static Settings ModSettings;
+        public const int normalMax = 29;
+        public const int absoluteMax = 64;
+        public const int casterMax = 60;
+        private static bool modEnabled = true;
+        private static bool tablePatched = false;
 
         static readonly Dictionary<Type, bool> typesPatched = new Dictionary<Type, bool>();
         static readonly List<String> failedPatches = new List<String>();
@@ -29,11 +46,15 @@ namespace CL29
         {
             if (logger != null) logger.Log(ex.ToString() + "\n" + ex.StackTrace);
         }
-        public static bool enabled;
         static bool Load(UnityModManager.ModEntry modEntry)
         {
             try
             {
+                ModSettings = UnityModManager.ModSettings.Load<Settings>(modEntry);
+                modEnabled = modEntry.Active;
+                modEntry.OnSaveGUI = OnSaveGui;
+                modEntry.OnToggle = OnToggle;
+                modEntry.OnGUI = OnGui;
                 logger = modEntry.Logger;
                 Main.DebugLog("Loading CL29");
                 var harmony = Harmony12.HarmonyInstance.Create(modEntry.Info.Id);
@@ -60,23 +81,45 @@ namespace CL29
         {
             /* add XP using the doubling-previous-level-need rule from PNP */
             /* beyond 29 impossible, as it requires > 2**31 XP (specifically, 2151900000) ... */
-            if (Game.Instance.BlueprintRoot.Progression.XPTable.Bonuses.Length < 30)
+            /* ... unless changing the scale */
+            // need to redo this every time, might have changed
             {
-                int thebase = Game.Instance.BlueprintRoot.Progression.XPTable.Bonuses[20];
-                int theincr = 2 * (thebase - Game.Instance.BlueprintRoot.Progression.XPTable.Bonuses[19]);
-                System.Array.Resize(ref Game.Instance.BlueprintRoot.Progression.XPTable.Bonuses, 30);
-                for (int i = 21; i <= 29; i++)
+                int thebase;
+                try
                 {
-                    thebase += theincr;
-                    theincr *= 2;
-                    Game.Instance.BlueprintRoot.Progression.XPTable.Bonuses.SetValue(thebase, i);
+                    thebase = Game.Instance.BlueprintRoot.Progression.XPTable.Bonuses[20];
+                }
+                catch (Exception e)
+                {// BlueprintLibrary.LibraryObject not loaded yet
+                    return false;
+                }
+                int theincr = 2 * (thebase - Game.Instance.BlueprintRoot.Progression.XPTable.Bonuses[19]);
+                int ctr = 0;
+                System.Array.Resize(ref Game.Instance.BlueprintRoot.Progression.XPTable.Bonuses, 1 + absoluteMax); // in case we switched back, full table
+                for (int i = 21; i <= absoluteMax; i++)
+                {
+                    if (!ModSettings.enableLinearScale)
+                    {
+                        thebase += theincr;
+                        theincr *= 2;
+                        if (i < 30)
+                            Game.Instance.BlueprintRoot.Progression.XPTable.Bonuses.SetValue(thebase, i);
+                        else
+                            Game.Instance.BlueprintRoot.Progression.XPTable.Bonuses.SetValue(0x7FFFFFFF - (absoluteMax - i), i);
+                    }
+                    else
+                    {
+                        ctr += (i - 20);
+                        Game.Instance.BlueprintRoot.Progression.XPTable.Bonuses.SetValue(thebase + theincr * ctr, i);
+                    }
                 }
             }
             /* extend the feat-per-character-level table */
-            if (Game.Instance.BlueprintRoot.Progression.FeatsProgression.LevelEntries.Length < 15)
+            //if (Game.Instance.BlueprintRoot.Progression.FeatsProgression.LevelEntries.Length < 15)
+            if (!tablePatched)
             {
-                System.Array.Resize(ref Game.Instance.BlueprintRoot.Progression.FeatsProgression.LevelEntries, 15);
-                for (int i = 10; i < 15; i++)
+                System.Array.Resize(ref Game.Instance.BlueprintRoot.Progression.FeatsProgression.LevelEntries, 20); // we stop at Lvl 40, otherwise the UI crashes
+                for (int i = 10; i < 20; i++)
                 {
                     Game.Instance.BlueprintRoot.Progression.FeatsProgression.LevelEntries[i] =
                         new LevelEntry();
@@ -92,12 +135,13 @@ namespace CL29
                 BlueprintCharacterClass sorcerer = ResourcesLibrary.TryGetBlueprint<BlueprintCharacterClass>(spontid);
                 if (sorcerer == null)
                     logger.Error($"Can't find blueprint # {spontid} for spontaneous caster.");
-                if (sorcerer != null && sorcerer.Spellbook.SpellsPerDay.Levels.Length < 30)
+                //if (sorcerer != null && sorcerer.Spellbook.SpellsPerDay.Levels.Length < (1 + casterMax))
+                if (sorcerer != null && !tablePatched)
                 {
                     logger.Warning($"Fixing Spells for {sorcerer.Name}");
-                    System.Array.Resize(ref sorcerer.Spellbook.SpellsPerDay.Levels, 30);
-                    System.Array.Resize(ref sorcerer.Spellbook.SpellsKnown.Levels, 30);
-                    for (int i = 21; i < 30; i++)
+                    System.Array.Resize(ref sorcerer.Spellbook.SpellsPerDay.Levels, (1 + casterMax));
+                    System.Array.Resize(ref sorcerer.Spellbook.SpellsKnown.Levels, (1 + casterMax));
+                    for (int i = 21; i < (1 + casterMax); i++)
                     {
                         sorcerer.Spellbook.SpellsPerDay.Levels[i] = new SpellsLevelEntry();
                         sorcerer.Spellbook.SpellsPerDay.Levels[i].Count = new int[10]; // going beyond LvL9 crashes the UI
@@ -109,7 +153,7 @@ namespace CL29
                         }
                         // add a Lvl9 + Lvl1, Lvl9 + Lvl2, etc. instead
                         sorcerer.Spellbook.SpellsPerDay.Levels[i].Count[9]++;
-                        sorcerer.Spellbook.SpellsPerDay.Levels[i].Count[i - 20]++;
+                        sorcerer.Spellbook.SpellsPerDay.Levels[i].Count[((i - 21) % 9) + 1]++;
 
                         sorcerer.Spellbook.SpellsKnown.Levels[i] = new SpellsLevelEntry();
                         sorcerer.Spellbook.SpellsKnown.Levels[i].Count = new int[10]; // going beyond LvL9 crashes the UI
@@ -133,11 +177,12 @@ namespace CL29
                 BlueprintCharacterClass wizard = ResourcesLibrary.TryGetBlueprint<BlueprintCharacterClass>(learnid);
                 if (wizard == null)
                     logger.Error($"Can't find blueprint # {learnid} for learned caster.");
-                if (wizard != null && wizard.Spellbook.SpellsPerDay.Levels.Length < 30)
+                //if (wizard != null && wizard.Spellbook.SpellsPerDay.Levels.Length < 41)
+                if (wizard != null && !tablePatched)
                 {
                     logger.Warning($"Fixing Spells for {wizard.Name}");
-                    System.Array.Resize(ref wizard.Spellbook.SpellsPerDay.Levels, 30);
-                    for (int i = 21; i < 30; i++)
+                    System.Array.Resize(ref wizard.Spellbook.SpellsPerDay.Levels, (1 + casterMax));
+                    for (int i = 21; i < (1 + casterMax); i++)
                     {
                         wizard.Spellbook.SpellsPerDay.Levels[i] = new SpellsLevelEntry();
                         wizard.Spellbook.SpellsPerDay.Levels[i].Count = new int[10]; // going beyond LvL9 crashes the UI
@@ -148,7 +193,7 @@ namespace CL29
                         }
                         // add a Lvl9 + Lvl1, Lvl9 + Lvl2, etc. instead
                         wizard.Spellbook.SpellsPerDay.Levels[i].Count[9]++;
-                        wizard.Spellbook.SpellsPerDay.Levels[i].Count[i - 20]++;
+                        wizard.Spellbook.SpellsPerDay.Levels[i].Count[((i - 21) % 9) + 1]++;
                     }
                 }
             }
@@ -160,12 +205,13 @@ namespace CL29
                 BlueprintCharacterClass bard = ResourcesLibrary.TryGetBlueprint<BlueprintCharacterClass>(learnid);
                 if (bard == null)
                     logger.Error($"Can't find blueprint # {learnid} for spontaneous half-caster.");
-                if (bard != null && bard.Spellbook.SpellsPerDay.Levels.Length < 30)
+                //if (bard != null && bard.Spellbook.SpellsPerDay.Levels.Length < 41)
+                if (bard != null && !tablePatched)
                 {
                     logger.Warning($"Fixing Spells for {bard.Name}");
-                    System.Array.Resize(ref bard.Spellbook.SpellsPerDay.Levels, 30);
-                    System.Array.Resize(ref bard.Spellbook.SpellsKnown.Levels, 30);
-                    for (int i = 21; i < 30; i++)
+                    System.Array.Resize(ref bard.Spellbook.SpellsPerDay.Levels, (1 + casterMax));
+                    System.Array.Resize(ref bard.Spellbook.SpellsKnown.Levels, (1 + casterMax));
+                    for (int i = 21; i < (1 + casterMax); i++)
                     {
                         bard.Spellbook.SpellsPerDay.Levels[i] = new SpellsLevelEntry();
                         bard.Spellbook.SpellsPerDay.Levels[i].Count = new int[7];
@@ -197,11 +243,12 @@ namespace CL29
                 BlueprintCharacterClass magus = ResourcesLibrary.TryGetBlueprint<BlueprintCharacterClass>(learnid);
                 if (magus == null)
                     logger.Error($"Can't find blueprint # {learnid} for learned half-caster.");
-                if (magus != null && magus.Spellbook.SpellsPerDay.Levels.Length < 30)
+                //if (magus != null && magus.Spellbook.SpellsPerDay.Levels.Length < 41)
+                if (magus != null && !tablePatched)
                 {
                     logger.Warning($"Fixing Spells for {magus.Name}");
-                    System.Array.Resize(ref magus.Spellbook.SpellsPerDay.Levels, 30);
-                    for (int i = 21; i < 30; i++)
+                    System.Array.Resize(ref magus.Spellbook.SpellsPerDay.Levels, (1 + casterMax));
+                    for (int i = 21; i < (1 + casterMax); i++)
                     {
                         magus.Spellbook.SpellsPerDay.Levels[i] = new SpellsLevelEntry();
                         magus.Spellbook.SpellsPerDay.Levels[i].Count = new int[7];
@@ -216,6 +263,7 @@ namespace CL29
                     }
                 }
             }
+            tablePatched = true;
             return true;
         }
 
@@ -226,13 +274,14 @@ namespace CL29
             // ReSharper disable once UnusedMember.Local
             private static bool Prefix(UnitEntityData unit, int __result)
             {
-                updateXPTable();
+                if (!tablePatched)
+                    updateXPTable();
                 unit = (unit ?? Game.Instance.Player.MainCharacter.Value);
                 int? num = (unit != null) ? new int?(unit.Descriptor.Progression.CharacterLevel) : null;
                 int i = (num == null) ? 1 : num.Value;
                 int? num2 = (unit != null) ? new int?(unit.Descriptor.Progression.Experience) : null;
                 int num3 = (num2 == null) ? 1 : num2.Value;
-                while (i < 29)
+                while (i < ModSettings.maxLevel)
                 {
                     if (Game.Instance.BlueprintRoot.Progression.XPTable.GetBonus(i + 1) > num3)
                     {
@@ -250,7 +299,8 @@ namespace CL29
         {
             private static bool Prefix(UnitDescriptor unit, ref bool __result)
             {
-                updateXPTable();
+                if (!tablePatched)
+                    updateXPTable();
                 __result = false;
                 if (Game.Instance.Player.IsInCombat)
                 {
@@ -261,7 +311,7 @@ namespace CL29
                     return false;
                 }
                 int characterLevel = unit.Progression.CharacterLevel;
-                if (characterLevel >= 29)
+                if (characterLevel >= ModSettings.maxLevel)
                 {
                     return false;
                 }
@@ -277,7 +327,8 @@ namespace CL29
         {
             private static bool Prefix()
             {
-                updateXPTable();
+                if (!tablePatched)
+                    updateXPTable();
                 return true;
             }
         }
@@ -288,7 +339,8 @@ namespace CL29
         {
             private static bool Prefix()
             {
-                updateXPTable();
+                if (!tablePatched)
+                    updateXPTable();
                 return true;
             }
         }
@@ -299,7 +351,8 @@ namespace CL29
         {
             private static bool Prefix()
             {
-                updateXPTable();
+                if (!tablePatched)
+                    updateXPTable();
                 return true;
             }
         }
@@ -314,6 +367,39 @@ namespace CL29
                 if (level > 20)
                     level = 41 - level;
                 return true;
+            }
+        }
+
+        private static void OnSaveGui(UnityModManager.ModEntry modEntry)
+        {
+            ModSettings.Save(modEntry);
+        }
+
+        private static bool OnToggle(UnityModManager.ModEntry modEntry, bool enabled)
+        {
+            modEnabled = enabled;
+            ModSettings.maxLevel = enabled ? (ModSettings.enableLinearScale ? absoluteMax : normalMax) : 20;
+            logger.Log($"CL29: limiting Character Level to {ModSettings.maxLevel}{(enabled ? (ModSettings.enableLinearScale ? " (linear scale)" : " (normal scale)") : " (-disabled-)")}");
+            updateXPTable();
+            return true;
+        }
+
+        private static void OnGui(UnityModManager.ModEntry modEntry)
+        {
+            try
+            {
+                UnityEngine.GUILayout.BeginHorizontal();
+                if (UnityEngine.GUILayout.Button($"{(ModSettings.enableLinearScale ? "<color=green><b>✔</b></color>" : "<color=red><b>✖</b></color>")} Enable a more linear XP scale (level max is {absoluteMax} with linear scale, {normalMax} without).", UnityEngine.GUILayout.ExpandWidth(false)))
+                {
+                    ModSettings.enableLinearScale = !ModSettings.enableLinearScale;
+                    ModSettings.maxLevel = modEnabled ? (ModSettings.enableLinearScale ? absoluteMax : normalMax) : 20;
+                    updateXPTable();
+                }
+                UnityEngine.GUILayout.EndHorizontal();
+            }
+            catch (Exception e)
+            {
+                modEntry.Logger.Error($"Error rendering GUI: {e}");
             }
         }
 
